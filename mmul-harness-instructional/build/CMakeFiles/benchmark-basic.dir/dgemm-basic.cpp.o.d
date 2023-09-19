205e2ecc36832ade87d1194ec0763a1f54627e6c
Kostanix <84185219+Kostanix@users.noreply.github.com>
2023-09-18 22:21:19 -0700

    Add files via upload

---
+++ mmul-harness-instructional/build/CMakeFiles/benchmark-basic.dir/dgemm-basic.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/benchmark-basic.dir/dgemm-basic.cpp.o: \
+ /mnt/hpc/mmul_harnes_instructional/mmul-harness-instructional/dgemm-basic.cpp \
+ /usr/include/stdc-predef.h
---
+++ mmul-harness-instructional/build/CMakeFiles/benchmark-blocked.dir/dgemm-blocked.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/benchmark-blocked.dir/dgemm-blocked.cpp.o: \
+ /mnt/hpc/mmul_harnes_instructional/mmul-harness-instructional/dgemm-blocked.cpp \
+ /usr/include/stdc-predef.h
